cd09c75dee2ea513ec765087be3a4d562d3ee69b
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-02-24 23:49:32 +0100

    Initial commit

---
+++ src/main/java/com/challenge/openaichallenge/OpenAiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.openaichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAiChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenge/openaichallenge/OpenAiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.openaichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAiChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
